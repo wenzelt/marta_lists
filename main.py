3b6f8cd707a6be451e3aed4b81331bb08c3a8e4f
Twenzel <tomwenzel@hotmail.de>
2021-11-28 20:43:49 +0100

    linting

--- main.py
+++ main.py
@@ -6,27 +6,29 @@ import datetime
 import os
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # reads list
     with open("list_short.txt") as f:
         short = f.readlines()
 
+    # reads list
     with open("list_long.txt") as f:
         long = f.readlines()
 
+    # Replaces newlines
+    short = [i.replace("\n", "") for i in short]
+    long = [i.replace("\n", "") for i in long]
+
+    # initializes empty list
     output = []
 
-    # for i in long:
-    #     long.append(i.replace('\n',""))
-    short = [i.replace('\n',"") for i in short]
-    long = [i.replace('\n',"") for i in long]
-
-
+    # matches substrings
     for short_line in short:
         for long_line in long:
             if short_line in long_line:
                 output.append(short_line) if short_line not in output else None
 
+    # writes to output.txt
     with open("output.txt", "w+") as f:
         for i in output:
-            f.write(str(i+"\n"))
-
+            f.write(str(i + "\n"))
